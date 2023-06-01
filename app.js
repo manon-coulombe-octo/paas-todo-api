a522a8644478a57a2848df2d2492d62af80eb791
Manon Coulombe <manon.coulombe@octo.com>
2023-06-01 14:43:21 +0200

    ajout environnement

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+const env = process.env.ENV
 app.get("/", function (req, res) {
-  res.send(`Hello World!`)
+  res.send(`Hello World! Environnement de ${env}`)
 })
 
 const port = process.env.PORT
